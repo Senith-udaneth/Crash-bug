a00e47a0f1fa951260a774331b2ad9ead79d3323
Senith-udaneth <senithudaneth025@gmail.com>
2025-07-08 08:35:25 +0530

    Create config.js

---
+++ settings/config.js
@@ -0,0 +1,4 @@
+global.prefa = ['','!','.',',','🐤','🗿']
+global.owner = ['94763007898']
+global.gambar = "https://files.catbox.moe/5gp130.jpg"
+// GANTI NO OWNER DENGAN NOMOR MU LALU RUNN SEPERTI BIASA !!!
